982107cb1638b11bf07823204b586d9a25261fce
Liaenis <84380297+Liaenis@users.noreply.github.com>
2022-01-03 13:39:41 +0100

    component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnTeteComponent } from './en-tete/en-tete.component';
+import { BarreNavComponent } from './barre-nav/barre-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnTeteComponent,
+    BarreNavComponent
   ],
   imports: [
     BrowserModule,
